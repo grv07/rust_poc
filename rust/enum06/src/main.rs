c6853893642386d93038c4b629480f5dfd7f8470
Gaurav Tyagi <tyagig@google.com>
2019-09-10 22:11:48 +0530

    remove buffer and update enums

--- rust/enum06/src/main.rs
+++ rust/enum06/src/main.rs
@@ -4,10 +4,45 @@ enum IpAddrKind {
     V6(String),
 }
 
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+enum Coin {
+    Penny,
+    Nickle,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) {
+    match coin {
+        Coin::Penny => {
+            println!("Penny here ...");
+            2
+        },
+        Coin::Nickle => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    };
+}
+
+fn add_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        Some(i) => Some(i + 1),
+        None => None,
+    }
+}
 
 fn main() {
     let home = IpAddrKind::V4(String::from("127.0.0.1"));
     let localhost = IpAddrKind::V6(String::from("::1"));
     println!("{:?}", home);
     println!("{:?}", localhost);
+    println!("{:?}", value_in_cents(Coin::Penny));
+    println!("{:?}", add_one(Option::Some(68)));
 }
