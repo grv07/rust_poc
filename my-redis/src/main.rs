749741f9ca3311e7d445cc574bacf28611e704e0
tyagig <grvtyagi22@gmail.com>
2020-12-06 22:42:19 +0530

    remove smps and create a async map to add key value storage via tokio :)

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -1,25 +1,50 @@
 use tokio::net::{TcpListener, TcpStream};
-use mini_redis::{Connection, Frame};
+use mini_redis::{Command, Connection, Frame};
+use mini_redis::Command::{Get, Set};
+use bytes::Bytes;
 
 use std::io;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>; 
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    let db:Db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
-        let (socket, _) = listener.accept().await?;
+        let (socket, _) = listener.accept().await.unwrap();
+        let clone_db = db.clone();
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, clone_db).await;
         });
     }
 }
 
-async fn process(stream: TcpStream) {
+async fn process(stream: TcpStream, db: Db) {
     let mut conn = Connection::new(stream);
-    if let Some(frame) = conn.read_frame().await.unwrap() {
-        println!("Frame: {:?}", frame);
 
-        let f_error = Frame::Error("unimplemented".to_string());
-        conn.write_frame(&f_error).await.unwrap();
+    while let Some(frame) = conn.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Get(cmd) => {
+                let db = db.lock().unwrap();
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone())
+                } else {
+                    Frame::Null
+                }            
+            }
+            Set(cmd) => {
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
+                Frame::Simple("OK".to_string())
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+        println!("{:?}", &response);
+        conn.write_frame(&response).await.unwrap();
     } 
 }
