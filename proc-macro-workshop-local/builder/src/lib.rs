2cff08a84a1455880d122da73b8708b461b0850d
tyagig <grvtyagi22@gmail.com>
2021-02-28 01:28:51 +0530

    add methods from struct

--- proc-macro-workshop-local/builder/src/lib.rs
+++ proc-macro-workshop-local/builder/src/lib.rs
@@ -1,31 +1,59 @@
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
 use quote::quote;
+use syn::Data::Struct;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let parse_input = parse_macro_input!(input as DeriveInput);
-    println!("{:#?}", parse_input);
-    let name = parse_input.ident;
-    let builder_name = format!("{}Builder", name);
-    println!("{}", builder_name);
+    let fields = if let Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+        ..
+    }) = parse_input.data
+    {
+        named
+    } else {
+        panic!("No field found in command")
+    };
+    let method = fields.iter().map(|field| {
+        let name = field.ident.as_ref();
+        let ty = &field.ty;
+        quote! {
+            fn #name(&self, #name:#ty) {}
+        }
+    });
+    let field_with_default = fields.iter().map(|field| {
+        let name = field.ident.as_ref();
+        let ty = &field.ty;
+        quote! {
+            #name: self.#name
+        }
+    });
 
+    let name = parse_input.ident;
     let command_builder = Ident::new(&format!("{}Builder", name), Span::call_site());
     let expansion = quote! {
         impl #name {
-            fn builder() -> #command_builder {
-                #command_builder {}
+            fn build(&self) -> #command_builder {
+                #command_builder {
+                    #(#field_with_default,)*
+                }
             }
         }
 
         struct #command_builder {
+           #fields
         }
+
         impl #command_builder {
-            fn executable(&self, executable:String) {}
-            fn env(&self, env: Vec<String>) {}
-            fn current_dir(&self, current_dir: String) {}
-            fn args(&self, args: Vec<String>) {}
+            fn builder(&self) -> #command_builder {
+                #command_builder {
+                }
+            }
+    }
+        impl #command_builder {
+            #(#method)*
         }
     };
     TokenStream::from(expansion)
