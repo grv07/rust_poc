aebc729db1327cafc090503af4d6902723c7ddb8
tyagig <grvtyagi22@gmail.com>
2021-02-28 22:46:41 +0530

    pass all test till 06

--- proc-macro-workshop-local/builder/src/lib.rs
+++ proc-macro-workshop-local/builder/src/lib.rs
@@ -2,11 +2,12 @@ use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
 use quote::quote;
 use syn::Data::Struct;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, DeriveInput, Field, Type, TypePath};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let parse_input = parse_macro_input!(input as DeriveInput);
+    eprintln!("{:#?}", parse_input);
     let fields = if let Struct(syn::DataStruct {
         fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
         ..
@@ -16,20 +17,65 @@ pub fn derive(input: TokenStream) -> TokenStream {
     } else {
         panic!("No field found in command")
     };
+
+    let is_field_optional = |field: &Field| {
+        let ty = &field.ty;
+        if let Type::Path(TypePath { ref path, .. }) = ty {
+            path.segments
+                .iter()
+                .any(|path_seg| path_seg.ident == "Option")
+        } else {
+            false
+        }
+    };
+
     let method = fields.iter().map(|field| {
         let name = field.ident.as_ref();
         let ty = &field.ty;
-        quote! {
-            fn #name(&mut self, #name:#ty) -> &mut Self {
-                self.#name = #name;
-                self
+        let is_op = is_field_optional(field);
+        if is_op {
+            quote! {
+                fn #name(&mut self, #name: String) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
+            }
+        } else {
+            quote! {
+                fn #name(&mut self, #name: #ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
             }
         }
     });
+
     let field_with_default = fields.iter().map(|field| {
         let name = field.ident.as_ref();
-        quote! {
-            #name: self.#name.clone()
+        let ty = &field.ty;
+        let is_op = is_field_optional(field);
+        if !is_op {
+            quote! {
+                #name: self.#name.take().unwrap().clone()
+            }
+        } else {
+            quote! {
+                #name: self.#name.clone()
+            }
+        }
+    });
+
+    let op_builder_fields = fields.iter().map(|field| {
+        let name = field.ident.as_ref();
+        let ty = &field.ty;
+        if is_field_optional(field) {
+            quote! {
+                #name: #ty
+            }
+        } else {
+            quote! {
+                #name: Option<#ty>
+            }
         }
     });
 
@@ -39,21 +85,21 @@ pub fn derive(input: TokenStream) -> TokenStream {
         impl #name {
             fn builder() -> #command_builder {
                 #command_builder {
-                    executable: "".to_string(),
-                    args: vec![],
-                    env: vec![],
-                    current_dir: "".to_string()
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None
                 }
             }
         }
 
         struct #command_builder {
-           #fields
+           #(#op_builder_fields,)*
         }
 
         impl #command_builder {
-            fn build(&self) -> std::result::Result<#command_builder, ()> {
-                Ok(#command_builder {
+            fn build(&mut self) -> std::result::Result<#name, ()> {
+                Ok(#name {
                     #(#field_with_default,)*
                 })
             }
