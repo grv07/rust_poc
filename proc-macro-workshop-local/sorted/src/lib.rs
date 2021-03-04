da9c14ed11910a50756ced367388b0a9e5b04c75
tyagig <grvtyagi22@gmail.com>
2021-03-05 00:33:07 +0530

    pass test 3 too :)

--- proc-macro-workshop-local/sorted/src/lib.rs
+++ proc-macro-workshop-local/sorted/src/lib.rs
@@ -6,12 +6,14 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
     let _ = input;
     println!("{:#?}", p_input);
+    // Checks if macro_attr is on an enum only.
     let is_item_enum = |item: &syn::Item| {
         if let syn::Item::Enum(syn::ItemEnum { .. }) = item {
             return true;
         }
         false
     };
+    // If Item is not enum then show an compile time error.
     if !is_item_enum(&p_input) {
         let error = syn::Error::new(
             proc_macro2::Span::call_site(),
@@ -21,5 +23,34 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
         return proc_macro::TokenStream::from(error);
     }
 
+    let variants = |item: &syn::Item| -> (Vec<syn::Ident>, Vec<String>) {
+        let mut var_vec = Vec::new();
+        let mut var_str_vec = Vec::new();
+        if let syn::Item::Enum(syn::ItemEnum { ref variants, .. }) = item {
+            for variant in variants.iter() {
+                var_vec.push(variant.ident.clone());
+                var_str_vec.push(variant.ident.to_string());
+            }
+        }
+        (var_vec, var_str_vec)
+    };
+
+    let (field_ident, field_name) = variants(&p_input);
+    let mut name_vec = field_name.clone();
+    name_vec.sort();
+
+    if name_vec != field_name {
+        for name in name_vec.iter().enumerate() {
+            if &field_name[name.0] != name.1 {
+                let span_idx = field_name.iter().position(|r| r == name.1).unwrap();
+                let error = syn::Error::new(
+                    field_ident[span_idx].span(),
+                    format!("{} should sort before {}", name.1, field_name[name.0]),
+                )
+                .into_compile_error();
+                return proc_macro::TokenStream::from(error);
+            }
+        }
+    }
     TokenStream::new()
 }
