f4988153b6c0bf34a84d723535a2711b8733b9f1
tyagig <grvtyagi22@gmail.com>
2021-03-05 22:23:36 +0530

    process furthur

--- proc-macro-workshop-local/main.rs
+++ proc-macro-workshop-local/main.rs
@@ -34,13 +34,26 @@
 
 use sorted::sorted;
 
+use std::fmt::{self, Display};
+use std::io;
+
 #[sorted]
-pub enum Conference {
-    RustBeltRust,
-    RustConf,
-    RustFest,
-    RustLatam,
-    RustRush,
+pub enum Error {
+    Fmt(fmt::Error),
+    Io(io::Error),
+}
+
+impl Display for Error {
+    #[sorted::check]
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        //use self::Error::*;
+
+        //#[sorted]
+        match self {
+            Io(e) => write!(f, "{}", e),
+            Fmt(e) => write!(f, "{}", e),
+        }
+    }
 }
 
 fn main() {}
